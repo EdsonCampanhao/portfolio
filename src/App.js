dd2d6438d7e0d569b69f449ef7f969a124491522
EdsonCampanhao <edsonlemos0525@gmail.com>
2023-03-27 17:12:41 -0300

    add
    adding footer section

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import About from 'components/About';
 import 'components/Header';
 import Header from 'components/Header';
 import Knowledges from 'components/Knowledges';
-import Projects from 'components/Projects'
+import Projects from 'components/Projects';
+import Footer from 'components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-  <header>
-     <Header itens={['Sobre','Habilidades','Projetos','Contatos']} />
-  </header>
-  <main>
-    <About/>
-    <Knowledges/>
-    <Projects/>
-  </main>
+      <header>
+        <Header itens={['Sobre', 'Habilidades', 'Projetos', 'Contatos']} />
+      </header>
+      <main>
+        <About />
+        <Knowledges />
+        <Projects />
+      </main>
+
+      <footer>
+        <Footer />
+      </footer>
+
     </div>
   );
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Footer.css'
+import {AiOutlineMail,AiFillGithub,AiFillLinkedin,AiOutlineWhatsApp} from 'react-icons/ai'
+
+const Footer = () => {
+  return (
+    <section className='footer'>
+        <h2>contatos</h2>
+        <address> Rio de Janeiro, São gonçalo</address>
+        <div className="footer__icons">
+            <AiOutlineMail size={32} fill='white'/>
+            <AiFillGithub size={32} fill='white'/>
+            <AiFillLinkedin size={32} fill='white'/>
+            <AiOutlineWhatsApp size={32} fill='white'/>
+        </div>
+
+
+    </section>
+  )
+}
+
+export default Footer;
