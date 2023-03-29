ecd471fc3a835ba74e80072014c29d943334b893
EdsonCampanhao <edsonlemos0525@gmail.com>
2023-03-29 16:48:14 -0300

    add new link

--- src/components/Knowledges/index.jsx
+++ src/components/Knowledges/index.jsx
@@ -1,4 +1,5 @@
 import './Knowledges.css'
+import Button from '@mui/material/Button';
 
 import icons from './icons.js'
 
@@ -7,11 +8,19 @@ const Knowledges = () => {
         <section className='knowledges'>
             {icons.map(icon =>
                 <article key={icon.name}>
-                    <p>{icon.name}</p>  
-                    <icon.icon size={32} className='icon'/>
+                    <p>{icon.name}</p>
+                    <icon.icon size={32} className='icon' />
                 </article>)}
+
+            <section className='button'>
+               <a href="https://cursos.alura.com.br/user/edsonlemos0525/fullCertificate/2323bcd680e7db8a229f724efe88a94e"><Button variant="outlined">ver cursos concluídos</Button></a> 
+            </section>
+
         </section>
     )
 }
 export default Knowledges;
+
+
+
 
